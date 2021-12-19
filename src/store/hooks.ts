f2b3ec71355fe460dc213f1924d9fe5584e5acee
Bryan Chen <xlchen1291@gmail.com>
2021-12-19 22:15:38 +1300

    bump versions

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,7 +1,8 @@
-import { Dispatch } from 'react'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { AnyAction } from 'redux'
-import type { RootState, AppDispatch } from './'
+import { Dispatch } from "react";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AnyAction } from "redux";
+import type { RootState, AppDispatch } from "./";
 
-export const useAppDispatch = (): Dispatch<AnyAction> => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch = (): Dispatch<AnyAction> =>
+  useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/utils/UtilsFunctions.ts
+++ src/utils/UtilsFunctions.ts
@@ -1,13 +1,9 @@
-import moment, { Moment } from "moment"
-import { encodeAddress } from "@polkadot/util-crypto/address"
-import {
-  BN,
-  BN_THOUSAND,
-  BN_TWO,
-} from "@polkadot/util"
-import { SubstrateAddress, TransformedSubstrateAddress } from "../types"
-import * as prefixes from "../utils/ss58-registry.json"
-import { ApiPromise } from "@polkadot/api"
+import moment, { Moment } from "moment";
+import { encodeAddress } from "@polkadot/util-crypto/address";
+import { BN, BN_THOUSAND, BN_TWO } from "@polkadot/util";
+import { SubstrateAddress, TransformedSubstrateAddress } from "../types";
+import * as prefixes from "../utils/ss58-registry.json";
+import { ApiPromise } from "@polkadot/api";
 
 export const replaceText = (
   key: string,
@@ -15,65 +11,65 @@ export const replaceText = (
   options: { ns: string; replace: Record<string, string> }
 ): string => {
   if (options.replace) {
-    let newValue = value
+    let newValue = value;
     for (const key in options.replace) {
-      newValue = newValue.replace(`{{${key}}}`, options.replace[key])
+      newValue = newValue.replace(`{{${key}}}`, options.replace[key]);
     }
-    return newValue
+    return newValue;
   }
-  return value || key
-}
+  return value || key;
+};
 
 export const transformDate = (
   unixTimestamp: number,
   transformUtc: boolean
 ): Moment => {
-  let auxMoment
+  let auxMoment;
   if (transformUtc) {
-    auxMoment = moment.utc(unixTimestamp)
+    auxMoment = moment.utc(unixTimestamp);
   } else {
-    auxMoment = moment(unixTimestamp)
+    auxMoment = moment(unixTimestamp);
   }
-  return auxMoment
-}
+  return auxMoment;
+};
 
 export const formatDate = (
   unixTimestamp: number,
   transformUtc: boolean,
   format = "YYYY-MM-DD HH:mm:ss"
 ): string => {
-  return transformDate(unixTimestamp, transformUtc).format(format)
-}
+  return transformDate(unixTimestamp, transformUtc).format(format);
+};
 
 export const toUnixTimestamp = (
   dateTime: Moment,
   transformUtc: boolean
 ): number => {
-  const simpleDateTime = dateTime.format("YYYY-MM-DD HH:mm:ss")
-  let timestamp
+  const simpleDateTime = dateTime.format("YYYY-MM-DD HH:mm:ss");
+  let timestamp;
   if (transformUtc) {
-    timestamp = moment.utc(simpleDateTime)
+    timestamp = moment.utc(simpleDateTime);
   } else {
-    timestamp = moment(simpleDateTime)
+    timestamp = moment(simpleDateTime);
   }
-  return timestamp.valueOf()
-}
+  return timestamp.valueOf();
+};
 
 export const transformAddress = (
   key: string
 ): TransformedSubstrateAddress[] => {
-  const publicKey = Uint8Array.from(Buffer.from(key.substring(2), "hex"))
-  const newFormats: TransformedSubstrateAddress[] = []
+  const publicKey = Uint8Array.from(Buffer.from(key.substring(2), "hex"));
+  const newFormats: TransformedSubstrateAddress[] = [];
   for (const auxPrefix of prefixes.registry) {
     if (auxPrefix.prefix !== 46 && auxPrefix.prefix !== 47) {
       newFormats.push({
         prefix: auxPrefix.prefix,
         value: encodeAddress(publicKey, auxPrefix.prefix),
-      })
+      });
     }
   }
-  return newFormats
-}
+  return newFormats;
+};
 
 export const findAuthorName = (
   savedAddresses: SubstrateAddress[],
@@ -82,12 +78,12 @@ export const findAuthorName = (
   for (const auxAddress of savedAddresses) {
     for (const auxTransformed of auxAddress.transformed) {
       if (auxTransformed.value === address) {
-        return auxAddress.name
+        return auxAddress.name;
       }
     }
   }
-  return undefined
-}
+  return undefined;
+};
 
 export const estimateStartBlockNumber = (
   endBlockNumber: number,
@@ -97,23 +93,23 @@ export const estimateStartBlockNumber = (
   weeks = 0,
   months = 0
 ): number => {
-  const auxMoment = moment()
-  const currentUnix = auxMoment.valueOf()
-  hours && auxMoment.subtract(hours, "hours")
-  days && auxMoment.subtract(days, "days")
-  weeks && auxMoment.subtract(weeks, "weeks")
-  months && auxMoment.subtract(months, "months")
-  const estimatedUnix = auxMoment.valueOf()
+  const auxMoment = moment();
+  const currentUnix = auxMoment.valueOf();
+  hours && auxMoment.subtract(hours, "hours");
+  days && auxMoment.subtract(days, "days");
+  weeks && auxMoment.subtract(weeks, "weeks");
+  months && auxMoment.subtract(months, "months");
+  const estimatedUnix = auxMoment.valueOf();
 
   return Math.max(
     1,
     endBlockNumber - (currentUnix - estimatedUnix) / estimatedBlockTime
-  )
-}
+  );
+};
 
-const EXPECTED_TIME_THRESHOLD = BN_THOUSAND.div(BN_TWO)
+const EXPECTED_TIME_THRESHOLD = BN_THOUSAND.div(BN_TWO);
 
-const EXPECTED_TIME_DEFAULT = new BN(6_000)
+const EXPECTED_TIME_DEFAULT = new BN(6_000);
 
 export const getExpectedBlockTime = (api: ApiPromise): number => {
   // https://github.com/polkadot-js/apps/blob/8ef4ed18dd281adfef3ce9e8f0bede8a82e62ec9/packages/react-hooks/src/useBlockTime.ts#L26
@@ -133,5 +129,5 @@ export const getExpectedBlockTime = (api: ApiPromise): number => {
         : // default guess for others
           EXPECTED_TIME_DEFAULT)
     ).toNumber()
-  )
-}
+  );
+};
